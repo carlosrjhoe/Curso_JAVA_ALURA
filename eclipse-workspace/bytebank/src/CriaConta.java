3dd57de4f477fd423de1ecfb4d817155c65788e6
Carlos Roberto <carlosrjhoe@gmail.com>
2021-09-13 20:15:56 -0300

    _130921_2015

--- eclipse-workspace/bytebank/src/CriaConta.java
+++ eclipse-workspace/bytebank/src/CriaConta.java
@@ -1,9 +1,9 @@
-public class CriaConta { 
+public class CriaConta {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 200; 
-        System.out.println(primeiraConta.saldo); 
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
 
         primeiraConta.saldo += 100;
         System.out.println(primeiraConta.saldo);
@@ -14,20 +14,7 @@ public class CriaConta {
         System.out.println("primeira conta tem " + primeiraConta.saldo);
         System.out.println("segunda conta tem " + segundaConta.saldo);
 
-        System.out.println(primeiraConta.agencia);
-        System.out.println(primeiraConta.numero);
-
-        System.out.println(segundaConta.agencia);
-
-        segundaConta.agencia = 146;
-        System.out.println("agora a segunda conta está na agencia " + segundaConta.agencia);
-
-        if(primeiraConta == segundaConta) {
-            System.out.println("mesma conta");
-        } else {
-            System.out.println("contas diferentes");
-        }
-        System.out.println(primeiraConta);
-        System.out.println(segundaConta);
+        System.out.println("referenciar: " + primeiraConta);
     }
+
 }
--- eclipse-workspace/bytebank/src/TestaMetodo.java
+++ eclipse-workspace/bytebank/src/TestaMetodo.java
@@ -1,25 +1,28 @@
-
 public class TestaMetodo {
-
 	public static void main(String[] args) {
-		
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(250.00);
-		System.out.println(conta.saldo);
-		
-		conta.saca(150.0);
-		System.out.println(conta.saldo);
-		
-		Conta contaJhoe = new Conta();
-		contaJhoe.deposita(200.00);
-		
-		if(contaJhoe.transfere(1000, conta)) {
-			System.out.println("Transferencia realizada!");
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+
+		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+
+		if (sucessoTransferencia) {
+			System.out.println("transferencia com sucesso");
+
 		} else {
-			System.out.println("faltou dinheiro!");
+			System.out.println("faltou dinheiro");
+
 		}
-		System.out.println(conta.saldo);
-		System.out.println(contaJhoe.saldo);
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
+
 }
