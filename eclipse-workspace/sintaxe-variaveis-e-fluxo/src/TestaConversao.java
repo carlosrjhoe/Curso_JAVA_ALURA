887d5c85dc9f5e1c22cd873e865d27de96aa08d5
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-19 18:11:54 -0300

    Trabalhando com caracteres

---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,15 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		int valor = (int) salario; // Mudar um valor double para int ou visse e versa.
+		System.out.println(salario);
+		System.out.println(valor);
+		
+		double num1 = 0.1;
+		double num2 = 0.2;
+		double total = num1 + num2;
+		System.out.println(total);
+	}
+}
--- eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -2,12 +2,12 @@
 public class TestaVariaveis {
 	public static void main(String[] args) {
 		
-		System.out.println("Ol· novamente!");
+		System.out.println("Ol√° novamente!");
 		
 		int idade1 = 36;
 		int idade2 = 37;
 		int soma = idade1 - idade2;
 		
-		System.out.println("A soma È = " + soma);
+		System.out.println("A soma √© = " + soma);
 	}
 }
