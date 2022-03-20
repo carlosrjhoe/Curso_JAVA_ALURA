7176582e7ac02da103e967ebb92093d1b0204385
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-19 23:43:34 -0300

    Composição de objetos

---
+++ eclipse-workspace/bytebanck-composto/src/TestaBanco.java
@@ -0,0 +1,31 @@
+
+public class TestaBanco {
+	public static void main(String[] args) {
+		Cliente carlos = new Cliente();
+		carlos.nome = "Carlos Roberto";
+		carlos.cpf = "060.199.314-47";
+		carlos.profissao = "Líder de manutenção";
+		
+		Cliente mayara = new Cliente();
+		mayara.nome = "Mayara Ramos";
+		mayara.cpf = "300.143.987-90";
+		mayara.profissao = "Boleira";
+		
+		Conta contaDoCarlos = new Conta();
+		contaDoCarlos.deposita(100);
+		
+		Conta contaDaMayara = new Conta();
+		contaDaMayara.deposita(100);
+		
+		contaDoCarlos.titular = carlos;
+		contaDaMayara.titular = mayara;
+		
+		System.out.println("Titular: " + contaDaMayara.titular.nome);
+		System.out.println("CPF: " + mayara.cpf);
+		System.out.println("Profissão: " + mayara.profissao);
+		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+		System.out.println("Titular: " + contaDoCarlos.titular.nome);
+		System.out.println("CPF: " + carlos.cpf);
+		System.out.println("Profissão: " + carlos.profissao);
+	}
+}
