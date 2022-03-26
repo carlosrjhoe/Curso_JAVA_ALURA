d0395cc49c71b3cd3b0dd75da5690e31926cf31b
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-26 16:43:09 -0300

    Atualizando arquivos do projeto

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-18/10/21
 
-Aprendendo uma nova linguagem de programação, JAVA.
+## Aprendendo uma nova linguagem de programação, JAVA.
 
 Este capítulo apresentou:
 
-O papel de um IDE e sua diferença de um editor
-Como fazer o download do Eclipse IDE
-Para que serve um workspace
-O conceito de perspectiva
-Como criar um projeto Java, inclusive classes e como executá-lo.
-Como exibir diferentes views
+- O papel de um IDE e sua diferença de um editor Como fazer o download do Eclipse IDE Para que serve um workspace O conceito de perspectiva Como criar um projeto Java, inclusive classes e como executá-lo. Como exibir diferentes views
---
+++ eclipse-workspace/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -0,0 +1,43 @@
+package br.com.alura;
+
+import java.awt.Desktop.Action;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+	
+	public static void main(String[] args) {
+		
+		String aula0 = "Adicionando conhecimento";
+		String aula1 = "Conhecendo listas";
+		String aula2 = "Modelando a classe Aula";
+		String aula3 = "Trabalhando com Cursos e Sets";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+		
+		// Percorrer uma lista
+		for (String aula : aulas) {
+			System.out.println(aula);
+		}
+		
+		// Percorrer uma lista
+		for(int i = 0; i < aulas.size(); i++) {
+			System.out.println("Aula: " + aulas.get(i));
+		}
+		
+		// Percorrer uma lista
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo : " + aula);
+		});
+		
+		// Ordenando lista
+		aulas.add(aula0);
+		Collections.sort(aulas);
+		System.out.println(aulas);
+	}
+}
